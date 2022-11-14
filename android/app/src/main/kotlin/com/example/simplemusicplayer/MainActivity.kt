07fd39398c331bb289be097a397082257aafa2b4
choiruladamm <choiruladamm@gmail.com>
2022-11-14 12:58:02 +0700

    simplemusicplayer

---
+++ android/app/src/main/kotlin/com/example/simplemusicplayer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simplemusicplayer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors
+// clone windows
 
 import 'dart:developer';
 
